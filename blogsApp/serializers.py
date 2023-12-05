9b5ab6b992f0cf3fe276ab7ae192da7d6f2a1de5
Mark Lewence Endrano <96058835+mrklwnc@users.noreply.github.com>
2023-12-05 11:03:54 +0800

    Added a CommentSerializer for testing

--- blogsApp/serializers.py
+++ blogsApp/serializers.py
@@ -1,9 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
         # fields = ['title']  # If you only want to pass the title field
+
+
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
--- blogsApp/tests.py
+++ blogsApp/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
 import requests
 
+from datetime import datetime
+
+from .serializers import CommentSerializer
+
 # Create your tests here.
 
-request = requests.get('http://127.0.0.1:8000/')
-print(request.json())
+# request = requests.get('http://127.0.0.1:8000/')
+# print(request.json())
+
+
+class Comment:
+    def __init__(self, email, content, created=None):
+        self.email = email
+        self.content = content
+        self.created = created or datetime.now()
+
+
+leila = Comment(email='leila@example.com', content='foo bar')
+serializer = CommentSerializer(leila)
+
+print("Comment: ", leila)
+print("Serialized Data: ", serializer.data)
